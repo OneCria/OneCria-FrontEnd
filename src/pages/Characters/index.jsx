d30a0b85b13d16b563e6d83b75e1cecc46cef171
Amerele <brunoplinks8@gmail.com>
2023-05-08 21:38:17 -0300

    characters page remaked

--- src/pages/Characters/index.jsx
+++ src/pages/Characters/index.jsx
@@ -1,4 +1,5 @@
 import { useState, useMemo } from "react";
+import { Container } from "./style";
 import axios from "axios";
 import {
   Accordion,
@@ -21,30 +22,40 @@ export function Characters() {
     setCharacters(data);
   });
 
+  const backgrounds = {
+    Atirador: { primary: "#806000", secundary: "#ffd966", third: "#bf8f00" },
+    //"Carateca Homem-Peixe": { primary: "#833c0b", secundary: "#9cc2e5" },
+    Ciborgue: { primary: "#833c0b", secundary: "#f4b083", third: "#ed7d31" },
+    Okama_Kenpo: { primary: "#833c0b", secundary: "#f4b083", third: "#ed7d31" },
+    Guerrilheiro: {primary:"#085452", secundary:"#8ef4bf", third: "#8ef4bf"}
+  };
+
   return (
-    <>
-      <Accordion allowMultiple>
+    <Container>
+      <h2>Seus personagens</h2>
+      <Accordion allowToggle id="accord">
         {characters.map((char) => (
           <AccordionItem key={char.id}>
-            <AccordionButton>
+            <AccordionButton
+              _expanded={{ bg: backgrounds[char.class]?.primary, color: "white" }}
+              bg={"#DBDFEA"}
+            >
               <h2>{char.name}</h2>
               <AccordionIcon />
             </AccordionButton>
             <AccordionPanel pb={4}>
-              <center>
-                <h1>Nome: {char.name}</h1>
-                <p>Raça: {char.race}</p>
-                <p>Classe: {char.class}</p>
-                <p>Nível: {char.level}</p>
-                <p>Profissão: {char.career}</p>
-                <p>Akuma no mi: {char.akuma ?? "Nenhuma"}</p>
-                <p>Vida máxima: {char.life}</p>
-                <p>Vida atual: {char.currentlife}</p>
-              </center>
+              <h1 style={{ backgroundColor: backgrounds[char.class]?.secundary }}>Nome: {char.name}</h1>
+                <p style={{ backgroundColor: backgrounds[char.class]?.third }}>Raça: {char.race}</p>
+                <p style={{ backgroundColor: backgrounds[char.class]?.secundary }}>Classe: {char.class}</p>
+                <p style={{ backgroundColor: backgrounds[char.class]?.third }}>Nível: {char.level}</p>
+                <p style={{ backgroundColor: backgrounds[char.class]?.secundary }}>Profissão: {char.career}</p>
+                <p style={{ backgroundColor: backgrounds[char.class]?.third }}>Akuma no mi: {char.akuma ?? "Nenhuma"}</p>
+                <p style={{ backgroundColor: backgrounds[char.class]?.secundary }}>Vida máxima: {char.life}</p>
+                <p style={{ backgroundColor: backgrounds[char.class]?.third }}>Vida atual: {char.currentlife}</p>
             </AccordionPanel>
           </AccordionItem>
         ))}
       </Accordion>
-    </>
+    </Container>
   );
 }
---
+++ src/pages/Characters/style.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+import theme from "../../styles/theme";
+
+export const Container = styled.div`
+  padding: 0 4rem;
+
+  #accord{
+    width:40rem;
+  }
+  p, h1{
+    margin: 0;
+    padding: 5px;
+    border: 1px solid #00000040;
+    font-weight: bold
+  }
+  
+`;
