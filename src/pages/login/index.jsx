96c9a72810a724cdb584a3b18a8cccf67472c95d
Bruno Gomes <88841840+amerele@users.noreply.github.com>
2023-05-16 13:29:17 -0300

    fixed: branch conflits

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -9,53 +9,11 @@ export const Login = () => {
 
   return (
     <Container>
-<<<<<<< HEAD
       {action == "Logar" ? (
         <SignIn actions={{ToggleCard, setToggleCard}}/>
       ) : (
         <SignUp actions={{ToggleCard, setToggleCard}}/>
       )}
-=======
-      <Card>
-        {/* ESQUERDA */}
-        <BoxLeft>
-          <div>
-            <div>
-              <label htmlFor="Usuario">
-                Nome de Usuário
-                <input
-                name="Usuario"
-                placeholder="Ex: Monkey D. Luffy"
-                onChange={(e) => {
-                  setUser(e.target.value);
-                  e.preventDefault();
-                }}
-              />
-                </label>
-            </div>
-            <div>
-              <label htmlFor="Senha">
-                Senha
-                <input
-                type="password"
-                name="Senha"
-                placeholder="Ex: S3nH4@"
-                onChange={(e) => {
-                  setPass(e.target.value);
-                  e.preventDefault();
-                }}
-              />
-                </label>
-            </div>
-            <button onClick={Logar}>Entrar</button>
-          </div>
-        </BoxLeft>
-        {/* DIREITA */}
-        <div>
-          <img src="src/media/login.png" alt="" />
-        </div>
-      </Card>
->>>>>>> 29b881c6c1e82a92af825daa20a21779b609fccd
     </Container>
   );
 };
